32836fc03416a56436ff4dc13ea9394ce199b77d
Naughty-ex <72936921+Naughty-ex@users.noreply.github.com>
2024-07-31 10:54:26 +0100

    modal

--- signup.js
+++ signup.js
@@ -142,7 +142,7 @@ function setupFormSubmission() {
     const modal = document.getElementById('successModal' + data.message);
     const span = document.getElementsByClassName('close')[0];
 
-    modal.style.display = "block";
+    // modal.style.display = "block";
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
